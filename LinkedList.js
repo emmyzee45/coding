c4874b619fd80698e41354a351c17972eca3594c
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2023-03-14 16:26:07 +0100

    end of linkedList

--- LinkedList.js
+++ LinkedList.js
@@ -61,4 +61,64 @@ class LinkedList {
     this.length--;
     return last;
   }
+
+  get(index) {
+    if(index >= this.length || index < 0) {
+      return null;
+    }
+
+    let count = 0; 
+    let current =  this.head;
+    while(count < index) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+
+  set(index, data) {
+    if(!this.get(index)) {
+      return false;
+    }
+
+    let node = this.get(index);
+    node.data = data;
+    return true;
+  }
+
+  remove(index){
+    if(!this.get(index)) {
+      return false;
+    }
+
+    if(index == 0) {
+      return this.shift();
+    }
+
+    const prevNode = this.get(index - 1);
+    const nodeToRemove = this.get(index);
+
+    prevNode.next = prevNode.next.next;
+    this.length--;
+    return nodeToRemove;
+  }
+
+  insert(index, data) {
+    if(index > this.length || index < 0) {
+      return false;
+    }
+
+    if(index === 0) {
+      this.unShift(index);
+      return true;
+    }
+
+    const prevNode = this.get(index - 1);
+    const nextNode = this.get(index);
+
+    prevNode.next = new Node(data, nextNode);
+    this.length++;
+    return true;
+
+  }
 }
