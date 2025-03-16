684f0f27a10b3a8bf88f816f5e1d0eb34301ed11
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2025-03-16 19:25:04 +0100

    all possible full binary trees

---
+++ neetcode/binaryTrees/possibleFullTrees.py
@@ -0,0 +1,29 @@
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val = 0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+
+        def backtrack(n):
+            if n == 0:
+                return []
+            if n == 1:
+                return [TreeNode()]
+            
+            res = []
+            for l in range(n):
+                r = n - 1 - l
+                leftTrees, rightTrees = backtrack(l), backtrack(r)
+                for t1 in leftTrees:
+                    for t2 in rightTrees:
+                        res.append(TreeNode(0, t1, t2))
+            return res
+        return backtrack(n)
+    
+obj = Solution()
+print(obj.allPossibleFBT(7))
