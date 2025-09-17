11ab8522757bfb6c91f2864ae9dbe7f8ec8ad3a1
emmyzee45 <emmyaondohemba45@gmail.com>
2025-09-17 22:55:07 +0100

    max matrix sum

---
+++ neetcode/graph/matrixSum.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        res = 0
+        neg_cnt = 0
+        mat_min = float('inf')
+        for row in matrix:
+            for n in row:
+                res += abs(n)
+                mat_min = min(mat_min, abs(n))
+                if n < 0:
+                    neg_cnt += 1
+
+        if neg_cnt & 1:
+            res -= 2 * mat_min
+        return res
+
+obj = Solution()
+print(obj.maxMatrixSum([[1,2,3], [-1,-2,-3],[1,2,3]]))
